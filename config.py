06e2dcc2ebd6fc2a6bdbbe20ac32bc30788bcb7e
Ayush <ayushraj2005.muz@gmail.com>
2025-12-27 11:40:20 +0530

    Setup config

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+
+CHUNK_SIZE = 512
+CHUNK_OVERLAP = 100
+
+TOP_K_RETRIEVAL = 5
+
+LLM_MODEL_NAME = "gpt-4.1-mini"
+
+DATA_PATH = "data/raw_docs"
